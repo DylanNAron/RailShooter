c67b7d1347b4bef562ddc8131c5c21f5986749e1
DylanNAron <arondylan72@gmail.com>
2020-07-07 16:55:20 -0400

    UI score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,13 +9,22 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] GameObject DeathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 7;
+
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
-
     }
 
 
@@ -27,6 +36,8 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
+
         GameObject fx = Instantiate(DeathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
